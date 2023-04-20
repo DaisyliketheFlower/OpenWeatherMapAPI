0216ace4a8ec545c9c462865dd938e78f727fe73
daisy.omoikirodah <thesmexxiichic@gmail.com>
2023-04-20 02:27:25 -0400

    done

---
+++ OpenWeatherMap/Program.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+using OpenWeatherMap;
+
+string key = File.ReadAllText("appsettings.json");
+string APIkey = JObject.Parse(key).GetValue("DefaultKey").ToString();
+
+Console.WriteLine("Please enter your zipcode");
+var zipCode = Console.ReadLine();
+
+string apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={APIkey}";
+
+Console.WriteLine();
+Console.WriteLine($"It is currently{WeatherMap.GetTemp(apiCall)}degrees F");
+
+
+
---
+++ OpenWeatherMap/WeatherMap.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenWeatherMap
+{
+	internal class WeatherMap
+	{
+		public static double GetTemp(string apiCall)
+		{
+			var client = new HttpClient();
+
+			var reponse = client.GetStringAsync(apiCall).Result;
+
+            var temp = double.Parse(JObject.Parse(reponse)["main"]["temp"].ToString());
+
+			return temp;
+		}
+	}
+}
+
